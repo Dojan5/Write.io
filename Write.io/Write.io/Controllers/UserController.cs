2eb959b5148966a501698769a4c7b2f71cab9b70
mantaboy <sigu77@gmail.com>
2018-05-22 14:56:12 +0200

    Add User Admin
    Option to edit/remove users blogs

---
+++ Write.io/Write.io/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Write.io.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+
+namespace Write.io.Controllers
+{
+
+    [Authorize]
+    public class UserController : Controller
+    {
+
+        ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public ActionResult Index()
+        {
+            var loggedInUser = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+
+            var model = db.Blogs.Where(b => b.User.UserName.Equals(loggedInUser.UserName))
+                        .OrderByDescending(b => b.Created).ToList();
+
+            return View(model);
+        }
+
+
+        public ActionResult RemoveBlog(int id)
+        {
+            Blog obj = db.Blogs.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+            db.Blogs.Remove(obj);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult EditBlog(int id)
+        {
+            Blog obj = db.Blogs.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
